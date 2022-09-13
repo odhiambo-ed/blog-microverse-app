7babc38af85e363209a668375daf2d2b0d1a65eb
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-13 18:18:26 +0300

    Add Likes Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_151503) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_151718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_151503) do
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
     t.integer "PostId"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
