2c81dba793b405680c22f807a4ab610975033d2f
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-15 11:40:25 +0300

    Add Method To Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  def update_posts_counter
+    user_id.update(posts_counter: author.posts.count)
+  end
+
+  def last_five_comments
+    comments.includes(:post_id).order(created_at: :ASC).limit(5)
+  end
 end
