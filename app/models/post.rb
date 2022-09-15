7b58e5fd6fbf5934c1bb04cc9ee1ee10e2b88cca
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-15 19:44:13 +0300

    Edit Linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'user', foreign_key: :AuthorId
-  has_many :likes,  class_name: 'like', foreign_key: :post_id
+  has_many :likes, class_name: 'like', foreign_key: :post_id
   has_many :comments, class_name: 'comment', foreign_key: :post_id
 
   def update_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, class_name: 'post', foreign_key: :AuthorId
-  has_many :likes class_name: 'like', foreign_key: :AuthorId
-  has_many :comments class_name: 'comment', foreign_key: :AuthorId
+  has_many :likes
+  has_many :comments
 
   def last_three_posts
     posts.includes(:user_id).order(created_at: :DESC).limit(3)
