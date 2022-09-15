eaa6a98b998a0075775a329c5ba1d751bf4f28bf
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-15 11:36:18 +0300

    Add Method To Return 3 Recent Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def last_three_posts
+    posts.includes(:user_id).order(created_at: :DESC).limit(3)
+  end
 end
