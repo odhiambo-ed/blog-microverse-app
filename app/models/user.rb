b5458ea22d14d1ffab5bbd9c618a2d6669e3bcc6
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-16 06:51:54 +0300

    Add Validations To User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :Name, presence: true, allow_blank: false
+  validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_three_posts
-    posts.includes(:user_id).order(created_at: :DESC).limit(3)
+    posts.includes(:AuthorId).order(created_at: :DESC).limit(3)
   end
 end
