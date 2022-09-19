4491654614d20806f59e274b797409350bb04cdb
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-19 16:40:08 +0300

    Add User Controller Spec

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'UserController', type: :request do
+  describe 'List Users' do
+    it "The method should render index.html.erb view" do
+      get '/user'
+      expect(response).to render_template('index')
+    end
+
+    it "the method should render a list of all users " do
+      get '/user'
+      expect(response.body).to include('list of all users')
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'Show' do
+    it 'should render show.html.erb view' do
+      get '/user/1'
+      expect(response).to render_template('show')
+    end
+
+    it 'should render a user by id' do
+      get '/user/1'
+      expect(response.body).to include('Get user by id')
+    end
   end
 end
