3e43e60be3b76d1ba08a58a44ff6447d3c012ca8
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-19 16:48:55 +0300

    Add Post controller Spec

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'List Posts' do
+    it "should render 'index.html.erb' view" do
+      get '/user/1/post'
+      expect(response.body).to include('list of posts for a given user')
+    end
+
+    it 'should render user/1/posts/index' do
+      get '/user/1/post'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'Show Post' do
+    it "should render 'show.html.erb' view" do
+      get '/user/1/post/1'
+      expect(response.body).to include('Get post by id')
+    end
+
+    it "should render 'show.html.erb' view" do
+      get '/user/1/post/1'
+      expect(response.body).to render_template('show')
+    end
   end
 end
