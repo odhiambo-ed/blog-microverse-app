c8352559174b639cedc5be964c3433cb9aec65fa
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-21 14:53:36 +0300

    Add Create Method To Post Controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,24 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.includes(:author).where(author: params[:user_id])
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.author = Current.user
+    if @post.save
+      flash[:notice] = 'Post was created successfully.'
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
