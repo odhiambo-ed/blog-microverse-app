387380b9e54fbfacad3cb22f3e546f3b471ca9b1
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-09-27 12:39:24 +0300

    Changed Users Table Column

---
+++ db/migrate/20220927093521_rename_user_posts_counter.rb
@@ -0,0 +1,5 @@
+class RenameUserPostsCounter < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :PostsCounter, :posts_counter
+  end
+end
--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,35 +1,80 @@
 require 'rails_helper'
 
-RSpec.describe 'Test User Index Page', type: :feature do
-  describe 'GET index' do
-    before(:each) do
-      @first_user = User.create(name: 'Rose', photo: 'image1.png', bio: 'This my first life story', posts_counter: 1)
-      @first_user.save!
-      @second_user = User.create(name: 'Lillian', photo: 'image2.png', bio: 'This my second life story', posts_counter: 5)
-      @second_user.save!
-      @third_user = User.create(name: 'Swine', photo: 'image3.png', bio: 'This my third life story', posts_counter: 4)
-      @third_user.save!
-    end
+# Test the users index page using integration tests
 
-    it 'shows the users username' do
-      visit '/'
-      expect(page).to have_content('Rose')
-      expect(page).to have_content('Lillian')
-      expect(page).to have_content('Swine')
-    end
-
-    it 'shows the users profile picture' do
-      visit '/'
-      expect(page).to have_css('img[src*="image1.png"]')
-      expect(page).to have_css('img[src*="image2.png"]')
-      expect(page).to have_css('img[src*="image3.png"]')
-    end
-
-    it 'shows the number of posts of each user' do
-      visit '/'
-      expect(page).to have_content('1')
-      expect(page).to have_content('5')
-      expect(page).to have_content('4')
-    end
+RSpec.describe 'Users Index Page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Swine', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
+    @user2 = User.create(name: 'Janet', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
   end
-end 
+  scenario 'displays all users username' do
+    visit '/'
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user2.name)
+  end
+
+  scenario 'displays all users profile photo' do
+    visit user_path(id: @user.id)
+    visit user_path(id: @user2.id)
+    find("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
+    find("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
+  end
+
+  scenario 'displays number of posts' do
+    visit '/'
+    expect(page).to have_content('Number of posts: 0')
+  end
+
+  scenario 'after clicking on a user, the user profile page is displayed' do
+    visit '/'
+    click_link @user.name
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user.bio)
+  end
+end
+
+RSpec.describe 'Users Show Page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Swine', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
+
+    5.times do |i|
+      Post.create(author: @user, title: "Post #{i}", text: 'This is my post')
+    end
+    visit user_path(id: @user.id)
+  end
+  scenario 'display user name and bio' do
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user.bio)
+  end
+
+  scenario 'displays user profile photo' do
+    find("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
+  end
+
+  scenario 'displays user number of post' do
+    expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+  end
+  scenario 'displays user first 3 posts' do
+    expect(page).to have_content('Post 4')
+    expect(page).to have_content('Post 3')
+    expect(page).to have_content('Post 2')
+    expect(page).to_not have_content('Post 1')
+  end
+
+  scenario 'button to view all posts is visible' do
+    find_button('See all posts')
+  end
+
+  scenario "should click a user's post, it redirects me to that post's show page." do
+    first('.post > a').click
+    expect(page).to have_content('Add a new comment')
+    find_button('like post')
+    expect(page).to have_content('Comments: 0 Likes: 0')
+  end
+
+  scenario 'go to the users post#index page when a user click on view all post' do
+    click_button('See all posts')
+    expect(page).to have_content(@user.bio)
+    expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+  end
+end
